18d7822423e872a47c8a9d5f803163faf9fb4a60
3xtreme-jay <jihoon@JiHoonui-MacBookPro.local>
2024-12-23 15:23:13 +0900

    Add TaskRepository to RepositoryModule

--- src/module/repository.module.ts
+++ src/module/repository.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { TaskRepository } from 'repository/TaskRepository'
 import { UserRepository } from 'repository/UserRepository'
 
 @Module({
   imports: [],
-  providers: [UserRepository],
-  exports: [UserRepository],
+  providers: [TaskRepository, UserRepository],
+  exports: [TaskRepository, UserRepository],
 })
 export class RepositoryModule {}
